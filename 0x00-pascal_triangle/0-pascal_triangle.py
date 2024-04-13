1cc314aba0d13fc9d328df50b1d4dd8f33df79ba
BOUZIDI Mohamed Ali <bouzidi.medali20@gmail.com>
2024-04-13 16:34:58 +0200

    fix code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,12 @@
+#!/usr/bin/python3
+""" Pascal Triangle """
+
+
 def pascal_triangle(n):
+    """Pascal Triangle"""
+    if n <= 0:
+        return []
+
     triangle = []
     for i in range(n):
         row = []
