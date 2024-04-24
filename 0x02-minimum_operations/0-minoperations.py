76e85b34539713ce964fdf27eb72e09f660dec49
BOUZIDI Mohamed Ali <bouzidi.medali20@gmail.com>
2024-04-25 01:13:22 +0200

    update script

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,19 +7,9 @@ def minOperations(n: int) -> int:
     A method that calculates the fewest number of operations
     needed to result in exactly n H characters in the file.
     """
-    content = "H"
-    copied = content * 2
-    number_of_operations = 0
-    while True:
-        if len(content) == n:
-            return number_of_operations
-        elif len(content) > n:
-            return 0
-        if len(content) * 2 > n:
-            number_of_operations += 1
-            content += copied
-            continue
-
-        number_of_operations += 2
-        copied = content
-        content = content * 2
+    if n <= 1:
+        return 0
+    for i in range(2, n + 1):
+        if n % i == 0:
+            return minOperations(n // i) + i
+    return 0
