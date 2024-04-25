082538b617d7ce74d0e84f9645470f876c40e44c
BOUZIDI Mohamed Ali <bouzidi.medali20@gmail.com>
2024-04-25 15:07:36 +0200

    update script

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,9 +7,37 @@ def minOperations(n):
     A method that calculates the fewest number of operations
     needed to result in exactly n H characters in the file.
     """
-    if n <= 1:
+    pasted_content = 1  # chars in the file
+    registry = 0  # H's copied
+    counter = 0  # operations counter
+
+    while pasted_content < n:
+        # if did not copy anything yet
+        if registry == 0:
+            registry = pasted_content
+            counter += 1
+
+        # if haven't pasted anything yet
+        if pasted_content == 1:
+            pasted_content += registry
+            counter += 1
+            continue
+
+        remaining = n - pasted_content  # remaining chars to Paste
+        if remaining < registry:
+            return 0
+
+        # if can't be devided
+        if remaining % pasted_content != 0:
+            pasted_content += registry
+            counter += 1
+        else:
+            registry = pasted_content
+            pasted_content += registry
+            counter += 2
+
+    # if got the desired result
+    if pasted_content == n:
+        return counter
+    else:
         return 0
-    for i in range(2, n + 1):
-        if n % i == 0:
-            return minOperations(n // i) + i
-    return 0
